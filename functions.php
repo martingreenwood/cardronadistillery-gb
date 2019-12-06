cc6671cb57991fce192763f1626d40f48aaf6c99
Martin G <martin@wearebeard.com>
2019-12-06 10:11:38 +0000

    removed redirtect js

--- functions.php
+++ functions.php
@@ -3,5 +3,5 @@
 add_action( 'wp_enqueue_scripts', 'cardrona_gb_styles' );
 function cardrona_gb_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_script( 'uk', get_stylesheet_directory_uri() . '/js/uk.js', array(), false, false );
+	// wp_enqueue_script( 'uk', get_stylesheet_directory_uri() . '/js/uk.js', array(), false, false );
 }
